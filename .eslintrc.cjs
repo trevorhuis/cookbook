6aa2a7640c9316c051fbdc9648bf7ff73fd71d4f
Trevor Huis in 't Veld <trevorh@hey.com>
2024-03-06 15:11:42 -0600

    Add prettier and tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,6 +33,7 @@ module.exports = {
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "plugin:jsx-a11y/recommended",
+        "prettier",
       ],
       settings: {
         react: {
@@ -69,6 +70,7 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:import/recommended",
         "plugin:import/typescript",
+        "prettier",
       ],
     },
 
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,31 +10,38 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div>
+      <h1 className="text-2xl">Welcome to Remix</h1>
       <ul>
         <li>
-          <a
+          <Link
+            className="text-sky-900 underline"
             target="_blank"
-            href="https://remix.run/tutorials/blog"
+            to="https://remix.run/tutorials/blog"
             rel="noreferrer"
           >
             15m Quickstart Blog Tutorial
-          </a>
+          </Link>
         </li>
         <li>
-          <a
+          <Link
+            className="text-sky-900 underline"
             target="_blank"
-            href="https://remix.run/tutorials/jokes"
+            to="https://remix.run/tutorials/jokes"
             rel="noreferrer"
           >
             Deep Dive Jokes App Tutorial
-          </a>
+          </Link>
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+          <Link
+            className="text-sky-900 underline"
+            target="_blank"
+            to="https://remix.run/docs"
+            rel="noreferrer"
+          >
             Remix Docs
-          </a>
+          </Link>
         </li>
       </ul>
     </div>
