3ccaaa6bbbdf81bc94c7574fbff120b2a7c8816e
Trevor Huis in 't Veld <trevorh@hey.com>
2024-03-19 16:24:05 -0500

    migration

--- migrations/0002_breezy_hex.sql
+++ migrations/0002_breezy_hex.sql
@@ -1,10 +1,10 @@
 -- Custom SQL migration file, put you code below! --
 CREATE TRIGGER recipes_fts_insert AFTER INSERT ON recipe
 BEGIN
-    INSERT INTO recipe_search (title, slug, description) VALUES (new.title, new.slug, new.description)
+    INSERT INTO recipe_search (title, slug, description) VALUES (new.title, new.slug, new.description);
 END;
 
 CREATE TRIGGER recipes_fts_delete AFTER DELETE ON recipe
 BEGIN
-    INSERT INTO recipe_search (recipe_search, title, slug, description) VALUES ('delete', old.title, old.slug, old.description)
+    INSERT INTO recipe_search (recipe_search, title, slug, description) VALUES ('delete', old.title, old.slug, old.description);
 END;
