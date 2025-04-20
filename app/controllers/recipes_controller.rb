cd78fd57ac10333400184e82733031dfc91174cd
Trevor Huisintveld <trevorhuisintveld@mac.lan>
2025-04-20 16:33:59 -0500

    create rails app

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,49 @@
+class RecipesController < ApplicationController
+  allow_unauthenticated_access only: %i[ index show ]
+  before_action :set_recipe, only: %i[ show edit update destroy ]
+
+  def index
+    @recipes = Recipe.page(params[:page]).per(8)
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+      params.expect(recipe: [:name, :content, :featured_image])
+    end
+end
